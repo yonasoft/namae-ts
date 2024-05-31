d72d911eeb15fce2e418ab6fb475c144e833bb33
KevC000 <chenkevin1119@gmail.com>
2024-05-31 06:47:03 -0400

    adjusted random logic for given names

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import fs from "fs";
 import { parse } from "papaparse";
 import { NameEntry, FullName } from "./types";
 
+// Load names from CSV file
 const loadNames = (): NameEntry[] => {
   const data = fs.readFileSync("src/assets/names.csv", "utf-8");
   const parsedData = parse<NameEntry>(data, {
@@ -13,16 +14,18 @@ const loadNames = (): NameEntry[] => {
 
 const names = loadNames();
 
+// Get a random name based on type and gender
 export const getRandomName = (
   type: "surname" | "given",
   gender: "male" | "female" | "all" = "all"
 ): NameEntry | null => {
   let filteredNames = names.filter((name) => name.type === type);
 
-  if (type === "given" && gender !== "all") {
-    filteredNames = filteredNames.filter(
-      (name) => name.gender === gender || name.gender === "all"
-    );
+  // Filter by gender if type is 'given' and gender is not 'all'
+  if (type === "given") {
+    if (gender !== "all") {
+      filteredNames = filteredNames.filter((name) => name.gender === gender);
+    }
   }
 
   if (filteredNames.length === 0) {
@@ -33,6 +36,7 @@ export const getRandomName = (
   return filteredNames[randomIndex];
 };
 
+// Get a random full name (given name + surname)
 export const getRandomFullName = (
   gender: "male" | "female" | "all" = "all"
 ): FullName | null => {
